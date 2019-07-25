1603401eb652549b71b76725733aa600dabc6a4f
aubry prieur <aubry.prieur@gmail.com>
2019-07-25 11:17:02 +0200

    setted GoogleMapReact with static info and created marker.jsx and bound in <GoogleMapReact />

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
+import GoogleMapReact from 'google-map-react';
+import Marker from './marker';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-  }
+  };
+
+  static defaultProps = {
+    center: {lat: 50.633333, lng: 3.066667},
+    zoom: 11
+  };
 
   render() {
     return (
@@ -13,7 +20,9 @@ class App extends Component {
 
         </div>
         <div className="map-container">
-
+          <GoogleMapReact defaultCenter={this.props.center} defaultZoom={this.props.zoom} >
+          <Marker lat={50.633333} lng={3.066667} text="My Marker" />
+        </GoogleMapReact>
         </div>
       </div>
       );
